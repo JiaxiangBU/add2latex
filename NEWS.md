0b060df2b821ba42605d9e3df5cd7a3535b9a86a
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-02-06 13:14:31 +0800

    * Added the function `add_latex`.

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+* Added the function `add_latex`.
+
 # add2latex 0.0.0.9000
 
 * Added a `NEWS.md` file to track changes to the package.
---
+++ R/add_latex.R
@@ -0,0 +1,31 @@
+#' Quick paste a LaTex object
+
+#' @author Jiaxiang Li
+#'
+#' @import rstudioapi
+#' @export
+
+add_latex <- function(type = 'bmatrix'){
+    text <-
+        if (type == 'bmatrix'){
+            glue::glue(
+            "
+            $$\\begin{bmatrix}
+            1 & 2 & 3
+            \\end{bmatrix}$$
+            "
+            ,.open = "<"
+            ,.close = ">"
+            )
+        }
+        if (type == 'more') {
+            cat("More types are under development, please click")
+            cat("https://jiaxiangli.netlify.com/2018/01/30/rmarkdown/#latex")
+        }
+    clipr::write_clip(text)
+    cat(
+        sep="\n"
+        ,text
+        ,tips()
+    )
+}
